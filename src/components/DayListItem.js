a4eda205f847593020d2241c3628da081737d9b8
Dkell88 <DarrenKelly1188@gmail.com>
2022-05-16 19:50:31 -0700

    Add daylist component. Test with storybook

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,10 +4,22 @@ import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
   
+  let dayClass = classNames("day-list__item", {
+    'day-list__item--selected':props.selected,
+    'day-list__item--full':(!props.spots)
+  })
+
+  const formatSpots = (spots) => {
+    if(!spots) return "no spots remaining"
+    if(spots === 1) return "1 spot remaining"
+    return `${spots} spots remaining`
+  }
+  const spotsRemaining = formatSpots(props.spots)  //Why can't this just be a function inside the h3 element
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{spotsRemaining}</h3>
     </li>
   );
 }
