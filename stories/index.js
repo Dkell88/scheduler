727db0f754d100fbdb59de7ab9c70ae2530562df
Dkell88 <DarrenKelly1188@gmail.com>
2022-05-16 21:24:23 -0700

    Refactor InterviewerList and item, removing prop drilling

--- stories/index.js
+++ stories/index.js
@@ -130,6 +130,6 @@ storiesOf("Button", module)
         .add("Clickable", () => (
           <InterviewerList
             interviewers={interviewers}
-            setInterviewer={action("setInterviewer")}
+            setInterviewer={() => action("setInterviewer")(interviewer.id)}
           />
         ));
