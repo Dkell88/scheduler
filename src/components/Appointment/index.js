f053f235fe95d8749461d2b1cbc2040febbc5575
Dkell88 <DarrenKelly1188@gmail.com>
2022-05-23 20:45:44 -0700

    Delete and edit functionity testing with errors

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -20,12 +20,10 @@ const EDIT = "EDIT"
 let statusMessage = "";
 
 export default function Appointment (props) {
-
-
- const {mode, transition, back } = useVisualMode(
-  props.interview ? SHOW : EMPTY
- );
-
+  
+  const {mode, transition, back } = useVisualMode(
+   props.interview ? SHOW : EMPTY
+  );
 
  const onAdd = () => {
   transition(CREATE)
@@ -68,14 +66,18 @@ export default function Appointment (props) {
 };
 
 const onConfirm = () => {
-  console.log("onConfirm")
+  console.log("onConfirm") 
   statusMessage = "Deleting"
-  transition(STATUS)
+  transition(STATUS,true)
   props.cancel(props.id)
   .then(() => {
     console.log("Index appoinmtment deleted")
     transition(EMPTY)
   })
+  .catch(err => {
+    console.log(err.message)
+    transition(ERROR, true)
+  });
 };
 
  const onDelete = () => {
@@ -90,7 +92,7 @@ const onConfirm = () => {
       {mode === SHOW && (<Show student={props.interview.student} interviewer={props.interview.interviewer} onDelete={onDelete} onEdit={() => transition(EDIT)}/>)}
       {mode === EMPTY && (<Empty onAdd={onAdd}/> )}
       {mode === CREATE && (<Form onSave={save} onCancel={onCancel}  interviewers={props.interviewers} onChange={onChange}/>)}
-      {mode === EDIT && (<Form onSave={save} onCancel={onCancel}  interviewers={props.interviewers} onChange={onChange} student={props.student} interviewer={props.interviewer}/>)}
+      {mode === EDIT && (<Form onSave={save} onCancel={onCancel}  interviewers={props.interviewers} onChange={onChange} student={props.interview.student} interviewer={props.interview.interviewer.id}/>)}
       {mode === STATUS && (<Status message={statusMessage}/> )}
       {mode === ERROR && (<Error onClose={onClose}/>)}
       {mode === CONFIRM && (<Confirm onCancel={onClose} onConfirm={onConfirm}/>)}
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,20 +4,23 @@ export default function useVisualMode(initial) {
   
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  
+ 
   function transition(nextMode, replace = false) {
     
     if(replace) {
-      setHistory((prev) => [...prev.slice(0, history.length-1)]);
+      setHistory((prev) => [...prev.slice(0, prev.length-1)]);
     }
-    setHistory((prev) => [...prev, nextMode]);
+    setHistory((prev) =>{
+      const newHist = [...prev, nextMode]
+      console.log("newHist ",newHist)
+       return [...prev, nextMode]
+      });
     setMode(nextMode);
-    setTimeout(() => {
-      console.log(history)}, 500)
+
   };
 
   function back() {
-    console.log(history)
+    console.log("Back: ", history)
     if(history.length === 1) return undefined
     setMode(history[history.length-2])
     setHistory((prev) => [...prev].slice(0, history.length-1));
