95e2775f155728bd36ab8cc2211aecbff1d335a4
Dkell88 <DarrenKelly1188@gmail.com>
2022-05-24 21:35:26 -0700

    Add tests for Form.js

--- src/components/InterviewList.js
+++ src/components/InterviewList.js
@@ -1,8 +1,9 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "./InterviewerList.scss"
+import PropTypes from 'prop-types';
 
-export default function InterviewerList(props) {
+const InterviewerList = (props) => {
 
   const interviewers = props.interviewers.map((interviewer) => {
     return (
@@ -26,10 +27,8 @@ export default function InterviewerList(props) {
   );
 }
 
-// const interviewers = [
-//   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-//   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-//   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-//   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-//   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-// ];
+// InterviewerList.propTypes = {
+//   interviewers: PropTypes.array.isRequired
+// };
+
+export default InterviewerList;
--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -6,6 +6,7 @@ import Button from "components/Button";
 
 afterEach(cleanup);
 
+
 it("renders without crashing", () => {
   render(<Button />);
 });
